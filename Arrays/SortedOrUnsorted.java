0553c2f97aadddfd7dbd46f49008b6fa4a83043d
MAK TECH <105592640+MAKTECH7@users.noreply.github.com>
2026-07-31 03:16:43 +0530

    Add files via upload

--- Arrays/SortedOrUnsorted.java
+++ Arrays/SortedOrUnsorted.java
@@ -1,5 +1,10 @@
 public class SortedOrUnsorted {
     public static void main(String[] args) {
-        
+        int[] arr = {1,2,5,4,9};
+        for(int i =0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+                System.out.println(arr[i+1]);
+            }
+        }
     }
 }
